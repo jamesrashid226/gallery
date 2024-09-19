fa40e2ce62ce21ce1bf7c611d5c40ce0eacce40c
jamesrashid226 <kali@kali.google>
2024-09-19 03:27:06 -1000

    a slight change in the connection server part

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -7,9 +6,7 @@ const path = require('path');
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-const mongoose = require('mongoose');
-
+// Connecting to the database
 mongoose.connect('mongodb+srv://james_rashid:james_rashid@cluster0.bwtll.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -19,34 +16,24 @@ mongoose.connect('mongodb+srv://james_rashid:james_rashid@cluster0.bwtll.mongodb
     console.error('Connection error:', err);
 });
 
-
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
-
 // Initializing the app
 const app = express();
-
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
-
+// Body parser middleware
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
+// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://0.0.0.0:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://0.0.0.0:${PORT}`);
 });
+
